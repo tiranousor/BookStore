23ddefa79780b3cfb2daf113cc122567adc4abe3
tiranousor <124511622+tiranousor@users.noreply.github.com>
2024-07-18 07:35:23 +0300

    Update OrderItemService.java

--- src/main/java/com/example/BookStore/services/OrderItemService.java
+++ src/main/java/com/example/BookStore/services/OrderItemService.java
@@ -1,11 +1,17 @@
 package com.example.BookStore.services;
 
 import com.example.BookStore.repositories.CartRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class OrderItemService {
-    private CartRepository orderItemRepository;
+    private final CartRepository orderItemRepository;
+
+    @Autowired
+    public OrderItemService(CartRepository orderItemRepository) {
+        this.orderItemRepository = orderItemRepository;
+    }
 
     public void deleteAll(int id){
         orderItemRepository.deleteAllByBookId(id);
